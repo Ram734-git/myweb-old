3368d5f9aab52bf7adee6c102a274081d9480686
GMLT059\Ramsudhakar <rama.sudhakar@graymatter.co.in>
2018-02-19 13:08:57 +0530

    modified files

--- scripts/server_startup.bat
+++ scripts/server_startup.bat
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command net stop httpd
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command net start httpd
 
-=======
-C:\Windows\WinSxS\wow64_microsoft-windows-powershell-exe_31bf3856ad364e35_6.3.9600.17415_none_6475a3ed0a50d87b\powershell.exe -Command net start httpd
->>>>>>> 5c2bd3ad2fbb62edd975a7c4c2a4488406753619
+
 
